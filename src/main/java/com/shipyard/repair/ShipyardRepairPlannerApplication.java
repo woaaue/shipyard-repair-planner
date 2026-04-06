7e733c20e77987a5dd110a600629768808a60500
woaaue <safat200@mail.ru>
2026-04-06 07:52:33 +0300

    Initial commit: Spring Boot project with JPA, Security, Flyway, Validation

---
+++ src/main/java/com/shipyard/repair/ShipyardRepairPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.shipyard.repair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipyardRepairPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShipyardRepairPlannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shipyard/repair/ShipyardRepairPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shipyard.repair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipyardRepairPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
