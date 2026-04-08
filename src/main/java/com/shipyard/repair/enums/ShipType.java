3951e7457d63b6f0dc7abf9c581d36d34eaef9a6
woaaue <124384635+woaaue@users.noreply.github.com>
2026-04-08 14:14:35 +0300

    add ship entity

---
+++ src/main/java/com/shipyard/repair/enums/ShipType.java
@@ -0,0 +1,14 @@
+package com.shipyard.repair.enums;
+
+public enum ShipType {
+    TANKER,
+    BULK_CARRIER,
+    CONTAINER_SHIP,
+    RO_RO,
+    PASSENGER,
+    FERRY,
+    TUG,
+    FISHING,
+    DREDGER,
+    OTHER;
+}
