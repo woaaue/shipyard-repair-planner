0816e0b7ae8c4390db52eb3883e5121c5af61e9a
woaaue <124384635+woaaue@users.noreply.github.com>
2026-04-11 14:09:00 +0300

    edit user dto

--- src/main/java/com/shipyard/repair/dto/user/CreateUserRequest.java
+++ src/main/java/com/shipyard/repair/dto/user/CreateUserRequest.java
@@ -1,15 +1,29 @@
 package com.shipyard.repair.dto.user;
 
 import com.shipyard.repair.enums.UserRole;
+import com.shipyard.repair.validation.ApacheEmail;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import org.hibernate.validator.constraints.Length;
+import jakarta.validation.constraints.Size;
 
 public record CreateUserRequest(
-        @NotNull @NotBlank @Length(max = 50) String email,
-        @NotNull @NotBlank @Length(max = 50) String firstname,
-        @NotNull @NotBlank @Length(max = 50) String lastname,
+        @NotBlank(message = "{user.email.blank}")
+        @Size(message = "{user.email.size}", min = 2, max = 50)
+        @ApacheEmail(message = "{user.email.invalid}")
+        String email,
+
+        @NotBlank(message = "{user.firstname.blank}")
+        @Size(message = "{user.firstname.size}", min = 2, max = 50)
+        String firstname,
+
+        @NotBlank(message = "{user.lastname.blank}")
+        @Size(message = "{user.lastname.size}", min = 2, max = 50)
+        String lastname,
+
         String patronymic,
-        @NotNull UserRole role,
+
+        @NotNull(message = "{user.role.not.null}")
+        UserRole role,
+
         Integer dockId
 ) {}
