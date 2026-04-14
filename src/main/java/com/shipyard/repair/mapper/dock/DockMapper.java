1f7de6b12aeac993c84711b345a530bcabea15b5
woaaue <124384635+woaaue@users.noreply.github.com>
2026-04-14 17:06:31 +0300

    add new method in dock mapper

--- src/main/java/com/shipyard/repair/mapper/dock/DockMapper.java
+++ src/main/java/com/shipyard/repair/mapper/dock/DockMapper.java
@@ -1,5 +1,6 @@
 package com.shipyard.repair.mapper.dock;
 
+import com.shipyard.repair.dto.dock.CreateDockRequest;
 import com.shipyard.repair.dto.dock.DockResponse;
 import com.shipyard.repair.entity.Dock;
 import org.mapstruct.Mapper;
@@ -9,4 +10,5 @@ import org.mapstruct.ReportingPolicy;
 public interface DockMapper {
 
     DockResponse toDto(Dock entity);
+    Dock toEntity(CreateDockRequest request);
 }
